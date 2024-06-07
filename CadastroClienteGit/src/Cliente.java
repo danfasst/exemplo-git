3117550953b854ff485e668f994c93753a07d282
SauloJL <saulolima_10@hotmail.com>
2024-06-07 10:44:59 -0300

    Classe Cliente criado - (Cuidado foi o estagiario junior que fez)

--- CadastroClienteGit/src/Cliente.java
+++ CadastroClienteGit/src/Cliente.java
@@ -39,7 +39,7 @@ public class Cliente {
 
     @Override
     public String toString() {
-        return "Nome=" + nome + ", telefone=" + telefone + ", email=" + email + "";
+        return "Nome=" + nome + ", telefone=" + telefone + ", email=" + email;
     }
 
     
